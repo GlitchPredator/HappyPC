ea28e0f8488880b56d7c89b012927c12b5fd3da1
Valentin Gheoace <valentin_gheoace@yahoo.com>
2025-03-16 23:54:17 +0200

    Added first frontend UX design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <p>
-      To be completed.
-    </p>
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div className="mx-auto p-5 col-6 shadow-lg modalStyle" style={{ width: "450px" }}>
+        <p className="h1">HappyPC 🧑‍💻</p>
+        <div className="w-100 text-start">
+          <p className="h6">ℹ️ Salutare, cu ce te putem ajuta?</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
