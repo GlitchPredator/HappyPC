90e2d23a3069febc57c9e8838e17c4ab82d36368
Valentin Gheoace <valentin_gheoace@yahoo.com>
2025-03-20 17:27:41 +0200

    Added animate.css and made use of it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import 'animate.css'
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="container vh-100 d-flex justify-content-center align-items-center">
-      <div className="mx-auto p-5 col-6 shadow-lg modalStyle" style={{ width: "450px" }}>
-        <p className="h1">HappyPC 🧑‍💻</p>
-        <div className="w-100 text-start">
-          <p className="h6">ℹ️ Salutare, cu ce te putem ajuta?</p>
+      <center>
+        <div className="mx-auto p-5 col-6 shadow-lg modalStyle animate__animated animate__fadeIn" style={{ width: "450px" }}>
+          <p className="h1 animate__animated animate__zoomIn">HappyPC 🧑‍💻</p>
+          <div className="w-100 text-start">
+            <p className="h6 mr-3 animate__animated animate__bounceIn animate__delay-1s">ℹ️ Salutare, eu sunt Happy, asistentul tău virtual! ☺️</p>
+            <p className="animate__animated animate__zoomIn animate__delay-2s">Dă-mi voie să te ajut să îți alegi un calculator.  😎</p>
+            <center><Button variant="danger" className="mt-3 animate__animated animate__fadeIn animate__delay-3s">Vreau un PC nou</Button></center>
+          </div>
         </div>
-      </div>
-    </div>
+      </center>
+    </div >
   );
 }
 
