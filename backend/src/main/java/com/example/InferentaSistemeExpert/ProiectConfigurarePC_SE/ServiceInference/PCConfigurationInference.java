3f08e7301aa600e3271180fa8d31049b4a46e5fc
Valentin Gheoace <valentin_gheoace@yahoo.com>
2025-04-10 03:02:01 +0300

    Added some fixes for backend logic

--- backend/src/main/java/com/example/InferentaSistemeExpert/ProiectConfigurarePC_SE/ServiceInference/PCConfigurationInference.java
+++ backend/src/main/java/com/example/InferentaSistemeExpert/ProiectConfigurarePC_SE/ServiceInference/PCConfigurationInference.java
@@ -10,7 +10,7 @@ public class PCConfigurationInference {
     //Metoda care gaseste PC-urile potrivite in functie de parametrii
     public String BackwardChaining(String name, String category, int ram, String storage, double price) throws IOException{
         for (PC pc : PcKnowledgeBase.pcs) {
-            if ((pc.getName().contains("ASUS") || pc.getName().contains("Myria") || pc.getName().contains("Lenovo")) && pc.getCategory().equals(category) && checkCharacteristics(pc, ram, storage, price)) {
+            if (pc.getName().contains((name)) && pc.getCategory().equals(category) && checkCharacteristics(pc, category, ram, storage, price)) {
                 return "PC recomandat: " + pc.getName() + " " + pc.getProcessor() + " " + pc.getRam() + " " + pc.getStorage() + " " + pc.getGpu() + " la pretul de " + pc.getPrice() + "RON.\nLink: " + pc.getLink();
             }
         }
@@ -18,12 +18,18 @@ public class PCConfigurationInference {
         return "Nu s-a gasit niciun PC recomandat!";
     }
 
-    private boolean checkCharacteristics(PC pc, int ram, String storage, double price) {
-
-        if(pc.getPrice() <= price &&
-                pc.getRam() >= ram &&
-                pc.getStorage().contains(storage)){
-            return true;
+    private boolean checkCharacteristics(PC pc, String category, int ram, String storage, double price) {
+        if (category.contains("office")) {
+            if ((price <= pc.getPrice() || price > pc.getPrice()) && pc.getStorage().contains(storage))
+                return true;
+        }
+        else if (category.contains("gaming")){
+            if ((price <= pc.getPrice()  || price > pc.getPrice()) && (pc.getName().contains("Myria") || pc.getName().contains("Lenovo") || pc.getName().contains("ASUS")))
+                return true;
+        }
+        else { // design
+            if ((price <= pc.getPrice() || price >= pc.getPrice()) && (pc.getRam() <= ram || pc.getRam() >= ram))
+                return true;
         }
 
         return false;
